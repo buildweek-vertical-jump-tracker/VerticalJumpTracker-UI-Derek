7ef28caa5a5b7c493d48cb6b899331496ab5151c
Derek S <dschwan21@me.com>
2019-05-22 23:55:16 -0700

    changed header texts and removed dunk image, index.html

--- JS/index.js
+++ JS/index.js
@@ -89,3 +89,14 @@ memImage.forEach(p =>
     alert("description");
   })
 );
+
+//scroll Nav Animation
+
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//     if (document.body.scrollTop > 96.8 || document.documentElement.scrollTop > 96.8) {
+//         document.getElementById("nav-container").style.opacity = "0.75";
+//     } else {
+//         document.getElementById("nav-container").style.opacity = "1";
+//     }
+// }
